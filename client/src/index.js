2cb886ba6dabd38f553cd1795919717bcf6e8087
raythurman2386 <raymondthurman5@gmail.com>
2019-11-10 19:13:01 -0500

    index refactored

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,14 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, withRouter } from 'react-router-dom';
+import App from './App';
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const AppWithRouter = withRouter(App);
+
+const rootElement = document.getElementById('root');
+render(
+  <Router>
+    <AppWithRouter />
+  </Router>,
+  rootElement
+);
