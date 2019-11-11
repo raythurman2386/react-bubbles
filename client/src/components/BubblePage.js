29947a5a19d9da74de30d4e3e378f0c7136a3237
raythurman2386 <raymondthurman5@gmail.com>
2019-11-10 20:46:08 -0500

    custom axios hook

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,19 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { axiosWithAuth as axios } from '../utils/axios';
+import React from 'react';
 
+import { useAxios } from '../hooks/useAxios';
 import Bubbles from './Bubbles';
 import ColorList from './ColorList';
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
-  useEffect(() => {
-    axios()
-      .get('/api/colors')
-      .then(res => setColorList(res.data))
-      .catch(err => console.log(err));
-  }, []);
+  const [colorList, setColorList] = useAxios();
 
   return (
     <>
