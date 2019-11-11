32b8cb174e1653e3a89847a5a94424fb92a6a20f
raythurman2386 <raymondthurman5@gmail.com>
2019-11-10 20:46:19 -0500

    refactored

--- client/src/App.js
+++ client/src/App.js
@@ -11,10 +11,6 @@ function App() {
     <div className='App'>
       <Route exact path='/' component={Login} />
       <PrivateRoute path='/bubblepage' component={BubblePage} />
-      {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
     </div>
   );
 }
